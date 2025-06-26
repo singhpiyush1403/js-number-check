e5d130ff4ee4315f6d11d175549cf597caea78ac
PIYUSH SINGH <singhpiyush1403@gmail.com>
2025-06-26 22:31:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+let number = 5;
+
+if (number > 0) {
+  console.log("The number is positive.");
+} else if (number < 0) {
+  console.log("The number is negative.");
+} else {
+  console.log("The number is zero.");
+}
